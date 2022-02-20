71e24f5e1ff46e8ffdfc5ea360250ca502e0107b
MacwithaQ <maqallaf@gmail.com>
2022-02-20 20:33:20 +0300

    recipe ingredient populate name

--- api/recipe/recipeControllers.js
+++ api/recipe/recipeControllers.js
@@ -2,7 +2,7 @@ const Recipe = require("../../database/models/Recipe");
 
 exports.getRecipe = async (req, res, next) => {
   try {
-    const recipe = await Recipe.find();
+    const recipe = await Recipe.find().populate("ingredients", ["name"]);
     return res.json(recipe);
   } catch (error) {
     next(error);

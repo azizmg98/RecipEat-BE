73d1dccadfb06b11b2bf4129fd81f8e3af2e5740
aziz <azizalghanim98@gmail.com>
2022-02-21 16:26:14 +0300

    add recipe to ingredient in controller

--- api/category/categoryControllers.js
+++ api/category/categoryControllers.js
@@ -1,6 +1,7 @@
 const Category = require("../../database/models/Category");
 const { findByIdAndDelete } = require("../../database/models/Recipe");
 const Recipe = require("../../database/models/Recipe");
+const Ingredient = require('../../database/models/Ingredient')
 
 exports.categoryGet = async (req, res, next) => {
   try {
@@ -34,9 +35,13 @@ exports.categoryDelete = async (req, res, next) => {
 exports.recipeCreate = async (req, res, next) => {
   try {
     const { categoryId } = req.params;
+    const ingredient = req.body.Ingredient
     req.body.category = categoryId;
     const newRecipe = await Recipe.create(req.body);
     await Category.findByIdAndUpdate(categoryId, {
+      $push: { recipes: newRecipe._id },
+    });
+    await Ingredient.findByIdAndUpdate(ingredient, {
       $push: { recipes: newRecipe._id },
     });
     return res.status(201).json(newRecipe);

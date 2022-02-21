5dc719b4be2ec6820b7101779d601dded8e7519f
MacwithaQ <maqallaf@gmail.com>
2022-02-21 15:22:45 +0300

    remove populate from getRecipe

--- api/recipe/recipeControllers.js
+++ api/recipe/recipeControllers.js
@@ -2,7 +2,7 @@ const Recipe = require("../../database/models/Recipe");
 
 exports.getRecipe = async (req, res, next) => {
   try {
-    const recipe = await Recipe.find().populate("ingredients", ["name"]);
+    const recipe = await Recipe.find();
     return res.json(recipe);
   } catch (error) {
     next(error);
